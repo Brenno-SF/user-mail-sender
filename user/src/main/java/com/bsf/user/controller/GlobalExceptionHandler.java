b7e700d47782be4dfabe72094126ce0a9b81bb8f
Brenno-SF <brprogrammersf@gmail.com>
2025-09-17 00:49:22 -0300

    adds more custom exceptions

--- user/src/main/java/com/bsf/user/controller/GlobalExceptionHandler.java
+++ user/src/main/java/com/bsf/user/controller/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.bsf.user.controller;
 
 import com.bsf.user.exceptions.EmailExistsException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,4 +20,32 @@ public class GlobalExceptionHandler {
         ErrorResponse error = ErrorResponse.create(ex, HttpStatus.CONFLICT, ex.getMessage());
         return ResponseEntity.status(409).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    String fieldName = error.getObjectName();
+                    String errorMessage = error.getDefaultMessage();
+                    errorResponse.put(fieldName, errorMessage);
+                });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
 }
